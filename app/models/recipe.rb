8fe2c4086aa3bfee638e3fa4499296fdb84a2995
christopherhuii <christopher.kam.hui@gmail.com>
2015-03-30 17:08:26 -0700

    fix error message handling

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -13,4 +13,6 @@ class Recipe < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
+  validates :ABV, presence: true
+
 end
